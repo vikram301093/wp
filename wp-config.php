2812dbbf48b696187ab701c1476b4284169186fa
SonamVerma9 <sonam.verma@atos.net>
2017-01-31 10:48:48 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,36 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME','wordpress');
+//define('DB_NAME','wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+//define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD',  'sonamverma');
+//define('DB_PASSWORD',  'sonamverma');
 
 /** MySQL hostname */
-define('DB_HOST', 'rds-mysql-sonam.cem5k1umd4qd.us-east-1.rds.amazonaws.com');
+//define('DB_HOST', 'rds-mysql-sonam.cem5k1umd4qd.us-east-1.rds.amazonaws.com');
+
+
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+$dbhost = getenv("MYSQL_DB_HOST");
+$dbname = getenv("MYSQL_DB_NAME");
+$dbuser = getenv("MYSQL_DB_USERNAME");
+$dbpwd = getenv("MYSQL_DB_PASSWORD");
+
+define('DB_NAME',$dbname);
+
+/** MySQL database username */
+define('DB_USER', $dbuser);
+
+/** MySQL database password */
+define('DB_PASSWORD',  $dbpwd);
+
+/** MySQL hostname */
+define('DB_HOST', $dbhost);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -46,6 +66,7 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
+/*
 define('AUTH_KEY',         '*|rNPCNCbx7}Ti3OnO~26D|uk@x[?|:AfOqwOPVK[TsId-|1o`~j>^fNR-8~#0A-');
 define('SECURE_AUTH_KEY',  '=a=~6GIaB_X|F?|+EkYBn*y,!?Z-0TXE.Pt>I`2*U-?`#5ab9Q5`9.c )uw7,*GE');
 define('LOGGED_IN_KEY',    '+G?x/+5I6U;OiejY5caBxH-tyW&<]3L!&O?+J)0*?+hS-;i$k]3acnkfJh4gDP0M');
@@ -53,9 +74,17 @@ define('NONCE_KEY',        'by[eR6~>?(7GRx{;.P,&5yKSg9q]IH5NViC<bJumayA>1@Ot%2):
 define('AUTH_SALT',        'shsA/U1_h>Vv|$)B##L+j+r5f9:m$5[^|7w>Qa R B+^?/28syL/Qpr(C6M+G4OG');
 define('SECURE_AUTH_SALT', 'u_NDg-glwKYGYUVPXj9V=1r+-S-U<vh82S=BDg=UoGo-O;$DEa/]4Y6+BFOF,),B');
 define('LOGGED_IN_SALT',   '0A4<8}rL]0vze?2hib/H}|(H=QPj~|R[Zfk!wy9-RY5LI>-W+Za{7h V`0ht!n;m');
-define('NONCE_SALT',       'NnRiQ<=fzPBxd-4Cg-b-H.Rjs Dy}+a/T2GQ%dmetb%^BlM+*#yeZtJ&*J>&eu-.');
-/**#@-*/
+define('NONCE_SALT',       'NnRiQ<=fzPBxd-4Cg-b-H.Rjs Dy}+a/T2GQ%dmetb%^BlM+*#yeZtJ&*J>&eu-.'); */
 
+/**#@-*/
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
 /**
  * WordPress Database Table prefix.
  *
